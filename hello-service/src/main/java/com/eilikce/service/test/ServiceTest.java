c6bc926d6497ee49282f66c56aa0e667100a0f69
wanghw <wanghw@wanghw-PC>
2018-02-08 14:27:20 +0800

    提交

---
+++ hello-service/src/main/java/com/eilikce/service/test/ServiceTest.java
@@ -0,0 +1,11 @@
+package com.eilikce.service.test;
+
+public class ServiceTest {
+	public void makeString(){
+		System.out.println("make String in Service !");
+	}
+	
+	public String createString(){
+		return "create a string !";
+	}
+}
--- hello-web/src/main/java/com/eilikce/hellow/test/Test.java
+++ hello-web/src/main/java/com/eilikce/hellow/test/Test.java
@@ -1,7 +1,11 @@
 package com.eilikce.hellow.test;
 
+import com.eilikce.service.test.ServiceTest;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
+		ServiceTest st = new ServiceTest();
+		st.makeString();
 	}
 }
